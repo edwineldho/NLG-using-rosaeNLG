4ef0e720190c987354d6fa8a9b9676cd46169fbe
edwineldho <64313653+edwineldho@users.noreply.github.com>
2022-06-29 16:30:27 +0800

    version2

--- intern.js
+++ intern.js
@@ -1,8 +1,8 @@
 //Generate facebook post text from product details, discounts, name and description
 
-// Declares the variable that 
-let request = ""
-
+// Declares the variable that are used to iterate through the array
+let request = "iPhone SE (2nd Gen)"
+let number  = 0
 
 //calls the rosaenlg library
 const rosaenlgPug = require('rosaenlg');
@@ -17,13 +17,33 @@ let pricings = [
       discount: 800,
       description: 'an internal RAM of 18GB, ROM 512GB Snapdragon 888 5G ',
     },
+    {
+      name: 'iPhone SE (2nd Gen)',
+      locations: ['Shoppee', 'Lazada', 'Amazon'],
+      pricing: 1199,
+      discount: 800,
+      description: '64GB ROM, 8GB RAM,  ',
+    },
+    {
+      name: 'Acer Swift 3 Infinity 4 SF314-511-57RH',
+      locations: ['Shoppee', 'Lazada', 'Amazon'],
+      pricing: 2540,
+      discount: 560,
+      description: 'Intel Core i5 RAM 16GB SSD 512GB ',
+    },
   ];
 
+
+  for (index = 0; index < pricings.length; index++) {
+    if (pricings[index].name = request) {
+    number = index;
+    }
+}
 
 //calls the tuto.pug
 let res = rosaenlgPug.renderFile('intern.pug', {
     language: 'en_US',
-    pricings: pricings,
+    pricing: pricings[number],
     cache: true,
   });
   console.log(res);
