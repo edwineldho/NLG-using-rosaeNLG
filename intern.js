91b7c77904180e796303b578d804dd35ce816ffa
edwineldho <64313653+edwineldho@users.noreply.github.com>
2022-07-05 17:54:07 +0800

    Combines NLP and NLG

--- intern.js
+++ intern.js
@@ -1,13 +1,11 @@
 //Generate facebook post text from product details, discounts, name and description
 
 // Declares the variable that are used to iterate through the array
-let request = "iPhone SE (2nd Gen)"
 let number  = 0
 let flag = false
 
 //calls the rosaenlg library
 const rosaenlgPug = require('rosaenlg');
-
 
 //This is the initial input which is used in order to generate the caption
 let pricings = [
@@ -35,23 +33,32 @@ let pricings = [
   ];
 
 
-for (index = 0; index < pricings.length; index++) {
-  if (pricings[index].name == request) {
-  number = index;
-  flag = true
-  }
-}
 
-if(flag == true){
-//calls the tuto.pug
-let res = rosaenlgPug.renderFile('intern.pug', {
-  language: 'en_US',
-  pricing: pricings[number],
-  cache: true,
-});
-console.log(res);
-}
 
-if(flag == false){
-  console.log("The input you have entered is not available")
-}
+// Using Your Saved Classifier
+var natural = require('natural');
+
+natural.BayesClassifier.load('nvclassifier.json',null,function(err,classifier){
+    let request = classifier.classify("Where can I get a new Asus ROG");
+    for (index = 0; index < pricings.length; index++) {
+      if (pricings[index].name == request) {
+      number = index;
+      flag = true
+      }
+    }
+    
+    if(flag == true){
+    //calls the tuto.pug
+    let res = rosaenlgPug.renderFile('intern.pug', {
+      language: 'en_US',
+      pricing: pricings[number],
+      cache: true,
+    });
+    console.log(res);
+    }
+    
+    if(flag == false){
+      console.log("The input you have entered is not available")
+    }
+})
+  
